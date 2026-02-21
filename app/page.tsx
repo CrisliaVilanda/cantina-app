8596b5a093f31a024dd3e5aa7a5d06523f81c629
CrisliaVilanda <crisliavieira@hotmail.com>
2026-02-21 11:14:46 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col mt-40 items-center justify-between p-24">
+      <h1 className="flex flex-col items-center text-3xl font-bold">Bem-vindo à Cantina José</h1>
+      <main className="flex flex-col items-center justify-between p-5">
+        <p>Esta é a página principal da cantina.</p>
+      </main>
+    </div>
+  );
+}
