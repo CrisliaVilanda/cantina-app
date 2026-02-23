aeff469ec08db64dcf9a0e966730b9ec56caa2c4
CrisliaVilanda <crisliavieira@hotmail.com>
2026-02-23 15:59:25 -0300

    add page admin, add btn atendente page

--- app/atendente/page.tsx
+++ app/atendente/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 export default function LoginAtendente(){
   return (
-    <h1>Página de Login do Atendente</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Página de Login do Atendente</h1>
+      <Link href="/admin" className="bg-blue-500 focus:bg-blue-700 text-white font-bold py-4 px-4 rounded">
+        Ir para Admin
+      </Link>
+    </div>
   )
 }
