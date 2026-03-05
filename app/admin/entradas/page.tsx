ee7bcced7a7a0cd271cdd0e8a9d18ae43fa823e0
Crislia FrontDev <crisliavieira@hotmail.com>
2026-03-05 19:33:33 +0000

    teste commit do codesandbox

--- app/admin/entradas/page.tsx
+++ app/admin/entradas/page.tsx
@@ -6,15 +6,15 @@ export default function EntradasPage() {
   return (
     <div className="container mx-auto px-4 py-4">
       <div className="flex justify-between items-center mb-6">
-       <div className="space-y-2">
-         <h1 className="text-3xl font-semibold">
-            Estoque de alimentos
+        <div className="space-y-2">
+          <h1 className="text-3xl font-semibold">
+            Estoque de alimentos alteração
           </h1>
-        <h3 className="text-xl font-light">Entradas</h3>
-       </div>
+          <h3 className="text-xl font-light">Entradas</h3>
+        </div>
         <Button asChild>
           <Link href="/admin/entradas/new">
-            <PlusCircle className="mr-2 h-5 w-5"/> Adicionar entrada
+            <PlusCircle className="mr-2 h-5 w-5" /> Adicionar entrada
           </Link>
         </Button>
       </div>
@@ -22,5 +22,5 @@ export default function EntradasPage() {
         <h3>Tabela</h3>
       </div>
     </div>
-  )
+  );
 }
