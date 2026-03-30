166aed93b5335e0e7b45e03cbd27363ecf9e652d
CrisliaVilanda <crisliavieira@hotmail.com>
2026-03-30 16:40:06 -0300

    formulário cardápio

--- app/components/forms/CardapioForm.tsx
+++ app/components/forms/CardapioForm.tsx
@@ -1,12 +1,15 @@
 "use client"
-import { Field, FieldLabel, FieldDescription, FieldError, FieldGroup, FieldLegend, FieldSet,   } from "@/components/ui/field"
+import { Field, FieldLabel, FieldDescription, FieldError, FieldGroup, FieldLegend, FieldSet, } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Controller, useForm } from "react-hook-form"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { NumericFormat } from "react-number-format"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import Link from "next/link"
 
 
 const formSchema = z.object({
@@ -142,28 +145,58 @@ export default function CardapioForm() {
                   render={({ field, fieldState }) => (
                     <Field>
                       <FieldLabel htmlFor="itemAnableMenu">Disponível no cardápio?</FieldLabel>
-                      <RadioGroup className="w-full max-w-xs"
+                      <RadioGroup
+                        className="flex flex-row gap-6"
                         value={field.value}
                         onValueChange={field.onChange}
                         aria-invalid={fieldState.invalid}>
-                        <Field orientation="horizontal">
-                          <RadioGroupItem value="sim" id="itemEnableMenu" />
-                          <FieldLabel htmlFor="itemEnableMenu">Sim</FieldLabel>
-                        </Field>
-                        <Field orientation="horizontal">
-                          <RadioGroupItem value="nao" id="itemDisanableMenu" />
-                          <FieldLabel htmlFor="itemDisanableMenu">Não</FieldLabel>
-                        </Field>
+                        <div>
+                          <Field orientation="responsive">
+                            <RadioGroupItem value="sim" id="itemEnableMenu" />
+                            <FieldLabel htmlFor="itemEnableMenu">Sim</FieldLabel>
+                          </Field>
+                        </div>
+                        <div>
+                          <Field orientation="responsive">
+                            <RadioGroupItem value="nao" id="itemDisanableMenu" />
+                            <FieldLabel htmlFor="itemDisanableMenu">Não</FieldLabel>
+                          </Field>
+                        </div>
                       </RadioGroup>
                     </Field>
                   )}
                 />
               </div>
             </div>
+            <div>
+              <Controller
+                name="itemDescription"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field>
+                    <FieldLabel htmlFor="itemDescription">Descrição deste item</FieldLabel>
+                    <Textarea {...field} aria-invalid={fieldState.invalid} id="itemDescription" placeholder="Servido em copo de 200ml" />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                    <FieldDescription>Informe os ingredientes desse item ou  como ele é servido</FieldDescription>
+                  </Field>
+                )}
+              />
+            </div>
           </FieldGroup>
+          <FieldGroup>
+            <div>
+              <Field orientation="horizontal">
+                <Button type="submit">Salvar</Button>
+                <Button asChild variant="outline" type="button">
+                  <Link href="/admin/cardapio/">Cancelar</Link>
+                </Button>
+              </Field>
+            </div>
+          </FieldGroup>
+
         </FieldSet>
-
-
       </form>
     </div>
   )
