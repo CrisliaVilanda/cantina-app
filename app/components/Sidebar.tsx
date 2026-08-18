e89acaf220afc777518181284477a33348c6dbe8
CrisliaVilanda <crisliavieira@hotmail.com>
2026-08-18 10:43:42 -0300

    WIP: ateração estoque -> estoque de alimentos + novo meunu

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -5,15 +5,16 @@ type SidebarProps = {
   sidebarOpen: boolean;
 };
 
-export default function Sidebar( {sidebarOpen} : SidebarProps) {
+export default function Sidebar({ sidebarOpen }: SidebarProps) {
 
   const menu = [
-    {title: "Dashboard", icon: LayoutDashboard, path: "/admin/dashboard"},
-    {title: "Estoque", icon: Package, path: "/admin/estoque"},
-    {title: "Gerenciar cardápio", icon: Utensils, path: "/admin/cardapio"},
-    {title: "Painel de pedidos", icon: ClipboardList, path: "/admin/painel"},
-    {title: "Relatório de vendas", icon: BarChart3, path: "/admin/relatorios"},
-    {title: "Configurações", icon: UserCog, path: "/admin/perfil"},
+    { title: "Dashboard", icon: LayoutDashboard, path: "/admin/dashboard" },
+    { title: "Estoque de alimentos", icon: Package, path: "/admin/estoque" },
+    { title: "Estoque de materiais", icon: Package, path: "/admin/estoque" },
+    { title: "Gerenciar cardápio", icon: Utensils, path: "/admin/cardapio" },
+    { title: "Painel de pedidos", icon: ClipboardList, path: "/admin/painel" },
+    { title: "Relatório de vendas", icon: BarChart3, path: "/admin/relatorios" },
+    { title: "Configurações", icon: UserCog, path: "/admin/perfil" },
   ]
 
   return (
@@ -24,21 +25,21 @@ export default function Sidebar( {sidebarOpen} : SidebarProps) {
 
       <nav className="flex-1 p-3">
         <ul className="flex- flex-col gap-2">
-       {menu.map((item) => {
-        const Icon = item.icon
-        return (
-          <li key={item.title} className={`${sidebarOpen ? "" : "flex items-center justify-center"}`}>
-            <Link 
-            href={item.path}
-            className={`flex items-center p-3 rounded-md hover:bg-blue-700 tezt-white transition ease-in-out ${sidebarOpen ? "justify-start gap-3 p-3" : "justify-center"}`}
-            >
-            <Icon size={20} className="shrink-0" />
-            <span className={`${sidebarOpen ? "block" : "hidden"}`}>{item.title}</span>
-            </Link>
+          {menu.map((item) => {
+            const Icon = item.icon
+            return (
+              <li key={item.title} className={`${sidebarOpen ? "" : "flex items-center justify-center"}`}>
+                <Link
+                  href={item.path}
+                  className={`flex items-center p-3 rounded-md hover:bg-blue-700 tezt-white transition ease-in-out ${sidebarOpen ? "justify-start gap-3 p-3" : "justify-center"}`}
+                >
+                  <Icon size={20} className="shrink-0" />
+                  <span className={`${sidebarOpen ? "block" : "hidden"}`}>{item.title}</span>
+                </Link>
 
-          </li>
-        )
-       })}
+              </li>
+            )
+          })}
         </ul>
 
 
